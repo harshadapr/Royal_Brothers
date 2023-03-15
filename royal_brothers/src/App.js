4c19f73e02ef6988d8ba7faeadb953c30dfcc9fa
Ankit Agrawal <ankitagrawal@Ankits-MacBook-Air.local>
2023-03-15 19:59:06 +0530

    complete payment page

--- royal_brothers/src/App.js
+++ royal_brothers/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Payment from './Components/Pages/Payment';
 
 
 
@@ -7,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <Payment />
     </div>
   );
 }
